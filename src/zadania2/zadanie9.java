a78e0333b244690e332b0647ddf5178e0ad80efc
Franio <franio.2115@example.com>
2022-11-08 11:50:16 +0100

    arkusz2

---
+++ src/zadania2/zadanie9.java
@@ -0,0 +1,22 @@
+package zadania2;
+
+import java.util.Scanner;
+
+public class zadanie9 {
+    public static void main(String[] args) {
+        Scanner klawiatura = new Scanner(System.in);
+        double waga;
+        double odleglosc;
+        System.out.println("podaj wage i odleglosc w km");
+        waga = klawiatura.nextDouble();
+        odleglosc = klawiatura.nextDouble();
+
+        if(waga <= 1){
+
+        }
+        //niedokonczone
+
+
+
+    }
+}
